0b4d03747db1c3b5e544a0eacba79aa28e69c623
Rafał Stępień <rafal.stepien@stxnext.pl>
2022-09-15 18:28:29 -0400

    Add config loader package

---
+++ config_loader/config_loader.py
@@ -0,0 +1,19 @@
+import os
+
+from pydantic import BaseSettings
+
+
+class ConfigLoader(BaseSettings):
+    HEROKU_POSTGRES_HOST: str = ""
+    HEROKU_POSTGRES_DATABASE: str = ""
+    HEROKU_POSTGRES_USER: str = ""
+    HEROKU_POSTGRES_PORT: int = 5432
+    HEROKU_POSTGRES_PASSWORD: str = ""
+
+    IPSTACK_ACCESS_KEY: str = ""
+
+    class Config:
+        env_file = os.environ.get("ENV_FILE")
+
+
+config = ConfigLoader()
