d4dc8ac7e8fe44f5d56fdbe7771d185bff9d546b
Rafał Stępień <rafal.stepien@stxnext.pl>
2022-09-16 11:06:25 -0400

    Add second test, extract fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,7 @@
 import pytest
+from database.database_client import DatabaseClient
+from tests.utils import load_test_json_data
+from ipstack_client.models import IpstackStandardLookupResponseModel
 
 
 @pytest.fixture
@@ -6,3 +9,24 @@ def create_engine_mock(mocker):
     return mocker.patch(
         "database.database_client.create_engine"
     )
+
+
+@pytest.fixture
+def test_database_client():
+    return DatabaseClient(
+        user="testuser",
+        password="testpassword",
+        host="127.0.0.1",
+        database="testdatabase",
+        port=2137,
+    )
+
+
+@pytest.fixture
+def test_ipstack_response_data():
+    return load_test_json_data("ipstack_response.json")
+
+
+@pytest.fixture
+def test_ipstack_response_object(test_ipstack_response_data):
+    return IpstackStandardLookupResponseModel(**test_ipstack_response_data)
--- tests/database_client/test_database_client.py
+++ tests/database_client/test_database_client.py
@@ -1,22 +1,18 @@
-from database.database_client import DatabaseClient
+from database.database_client import Session
 
 
-def test_database_client_initializes_correctly(create_engine_mock):
-
-    client = DatabaseClient(
-        user="testuser",
-        password="testpassword",
-        host="127.0.0.1",
-        database="testdatabase",
-        port=2137,
-    )
-
-    assert client.connection_string == "postgresql://testuser:testpassword@127.0.0.1/testdatabase"
+def test_database_client_initializes_correctly(create_engine_mock, test_database_client):
+    assert test_database_client.connection_string == "postgresql://testuser:testpassword@127.0.0.1/testdatabase"
     assert create_engine_mock.called
 
 
-def test_upload_data_inserts_to_database():
-    pass
+def test_upload_data_inserts_to_database(mocker, create_engine_mock, test_database_client, test_ipstack_response_object):
+    session_add_mock = mocker.patch.object(Session, "add")
+    session_commit_mock = mocker.patch.object(Session, "commit")
+    test_database_client.upload_data(test_ipstack_response_object)
+
+    assert session_add_mock.called
+    assert session_commit_mock.called
 
 
 def test_client_handles_database_connection_error_correctly():
