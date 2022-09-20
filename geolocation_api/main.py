512c6cb23fbd487e524d2079e3862ddc47d608ce
Rafał Stępień <rafal.stepien@stxnext.pl>
2022-09-20 10:27:32 -0400

    Add response model for token endpoint

--- geolocation_api/main.py
+++ geolocation_api/main.py
@@ -3,7 +3,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from config_loader.config_loader import config
 from database.database_client import DatabaseClient
-from geolocation_api.models import JWTData
+from geolocation_api.models import JWTData, TokenResponseModel
 from geolocation_api.security import authenticate_user, decode_jwt_authorization_header, encode_jwt_token
 from ipstack_client.ipstack_client import IpstackClient
 
@@ -18,19 +18,19 @@ database_client = DatabaseClient(
 )
 
 
-@app.get("/get_data")
+@app.get("/data")
 def get_data(ip_address: str):
     return {"endpoint": "get_data"}
 
 
-@app.post("/add_data")
+@app.post("/data")
 def add_data(ip_address: str, jwt_data: JWTData = Depends(decode_jwt_authorization_header)):
     data = ipstack_client.get_data_for_ip_address(ip_address)
     database_client.upload_data(data)
     return Response(status_code=status.HTTP_201_CREATED)
 
 
-@app.get("/delete_data")
+@app.delete("/data")
 def delete_data(ip_address: str):
     return {"endpoint": "delete_data"}
 
@@ -39,10 +39,4 @@ def delete_data(ip_address: str):
 def post_credentials_to_obtain_jwt_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = database_client.get_user(form_data.username)
     authenticate_user(user, form_data.password)
-    return {
-        "jwt_token": encode_jwt_token(
-            {
-                "username": user.username,
-            }
-        )
-    }
+    return TokenResponseModel(token=encode_jwt_token({"username": user.username}))
